23aecc86772d418b8a6fe60569004621a24d840b
polarbroadband <polarbroadband@gmail.com>
2022-04-14 22:48:14 -0400

    comm

--- src/greeting/greeting.go
+++ src/greeting/greeting.go
@@ -5,22 +5,46 @@ import (
 	"fmt"
 	"net/http"
 	"os/exec"
+	"strconv"
+	"strings"
 
 	log "github.com/sirupsen/logrus"
 )
 
 func greeting(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	log.Infof("GET request")
 
-	cmd := exec.Command("python", "-c", "import pt; print(pt.ingredients('foo', 'bar'))")
-	fmt.Println(cmd.Args)
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		fmt.Println(err)
+	if dish, ok := r.URL.Query()["m"]; ok && len(dish) > 0 {
+		if serve, ok := r.URL.Query()["s"]; ok && len(serve) > 0 {
+			if intVar, err := strconv.Atoi(serve[0]); err == nil { // convert string to int
+				// call function <<ingredients>> in python module <<pt.py>>
+				// passing arguments
+				cmd := exec.Command("python", "-c", fmt.Sprintf("import pt; print(pt.ingredients('%v', %v))", dish[0], intVar))
+				fmt.Println(cmd.Args)
+				// retrieve pthon function return values
+				if out, err := cmd.CombinedOutput(); err == nil {
+					// replace ' with "
+					// remove \n from the end of string
+					ss := strings.ReplaceAll(strings.TrimSpace(string(out)), `'`, `"`)
+					fmt.Println(ss)
+					var resp map[string]string
+					if err := json.Unmarshal([]byte(ss), &resp); err == nil {
+						if err := json.NewEncoder(w).Encode(resp); err != nil {
+							http.Error(w, err.Error(), 500)
+						}
+					} else {
+						fmt.Println(err)
+					}
+					return
+				} else {
+					fmt.Println(err)
+				}
+			} else {
+				fmt.Println(err)
+			}
+		}
 	}
-	fmt.Println(string(out))
-
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	if err := json.NewEncoder(w).Encode(map[string]string{"from": `T-T`, "status": "ALL GOOD"}); err != nil {
 		http.Error(w, err.Error(), 500)
 	}
--- src/greeting/pt.py
+++ src/greeting/pt.py
@@ -3,6 +3,6 @@ import sys
 def ingredients(dish, serve):
     return {
         "dish": dish,
-        "pork_belly": str(0.25*serve)+"kg",
-        "dry_pea_pod": str(0.05*serve)+"kg",
+        "pork_belly": str(0.25*float(serve))+"kg",
+        "dry_pea_pod": str(0.05*float(serve))+"kg",
     }
